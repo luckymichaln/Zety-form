baed8aa67f72491fb5fe3c7c7fd83e729cbba19e
luckymichaln <michalnowak.contact@gmail.com>
2019-10-23 11:58:19 +0200

    create logic Form component

---
+++ src/components/Form.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      form: null
+    }
+  }
+
+  render() {
+    return (
+      <form>form</form>
+    )
+  }
+}
+
+export default Form
